d7af5382ccc560f46d4673d96ac66a5ca5085ce0
dhuangcodes <dhuang3@andrew.cmu.edu>
2026-04-20 02:35:42 -0400

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-# Per-category Discord webhooks
 WEBHOOK_NBA         = os.getenv("WEBHOOK_NBA", "")
 WEBHOOK_MLB         = os.getenv("WEBHOOK_MLB", "")
 WEBHOOK_TENNIS      = os.getenv("WEBHOOK_TENNIS", "")
 WEBHOOK_VIDEOGAMES  = os.getenv("WEBHOOK_VIDEOGAMES", "")
 WEBHOOK_OTHER       = os.getenv("WEBHOOK_OTHER", "")
+DISCORD_BOT_AUTH    = os.getenv("DISCORD_BOT_AUTH", "")
 
-MIN_TRADE_USD       = float(os.getenv("MIN_TRADE_USD", "5000"))
+MIN_TRADE_USD       = float(os.getenv("MIN_TRADE_USD", "3000"))
 POLL_INTERVAL       = int(os.getenv("POLL_INTERVAL", "45"))
 TOP_WALLETS_COUNT   = int(os.getenv("TOP_WALLETS_COUNT", "300"))
